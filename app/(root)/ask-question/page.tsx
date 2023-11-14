c2a873e91262db7462c3c461b233af174e933d7f
Akaikenlol <keithken972@gmail.com>
2023-11-15 01:01:57 +0630

    second commit

---
+++ app/(root)/ask-question/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Ask = () => {
+	return (
+		<main className="flex justify-center items-center text-center min-h-screen">
+			Ask Questions
+		</main>
+	);
+};
+
+export default Ask;
