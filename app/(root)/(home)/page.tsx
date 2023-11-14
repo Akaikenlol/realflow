ad4f6f8248eab75409826315f10489a3dc24386b
Akaikenlol <keithken972@gmail.com>
2023-11-15 00:57:41 +0630

    first commit

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+	return (
+		<main className="flex justify-center items-center text-center min-h-screen text-emerald-400">
+			Home
+		</main>
+	);
+};
+
+export default Home;
