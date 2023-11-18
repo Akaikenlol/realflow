2dafeabd4a126172033a232609febfcce397a90a
Akaikenlol <keithken972@gmail.com>
2023-11-19 00:10:35 +0630

    navbar implementation

---
+++ components/shared/navbar/Theme.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import React from "react";
+import { useTheme } from "@/context/ThemeProvider";
+import {
+	Menubar,
+	MenubarContent,
+	MenubarItem,
+	MenubarMenu,
+	MenubarSeparator,
+	MenubarShortcut,
+	MenubarTrigger,
+} from "@/components/ui/menubar";
+import Image from "next/image";
+
+const Theme = () => {
+	const { mode, setMode } = useTheme();
+	return (
+		<Menubar className="relative border-none shadow-none bg-transparent">
+			<MenubarMenu>
+				<MenubarTrigger className="focus:bg-light-900 data-[status=open]:bg-light-900 dark:focus:bg-dark-200 dark:data-[status-open]:bg-dark-200">
+					{mode === "light" ? (
+						<Image
+							src="/assets/icons/sun.svg"
+							alt="sun"
+							width={20}
+							height={20}
+							className="active-theme"
+						/>
+					) : (
+						<Image
+							src="/assets/icons/moon.svg"
+							alt="moon"
+							width={20}
+							height={20}
+							className="active-theme"
+						/>
+					)}
+				</MenubarTrigger>
+				<MenubarContent className="absolute right-[-3rem] mt-3 min-w-[120px] py-2 rounded border dark:border-dark-400 dark:bg-dark-300"></MenubarContent>
+			</MenubarMenu>
+		</Menubar>
+	);
+};
+
+export default Theme;
