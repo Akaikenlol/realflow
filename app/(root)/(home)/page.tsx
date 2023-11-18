27ed2c56b43b5d2944f61e4f1654fba61bc77d22
Akaikenlol <keithken972@gmail.com>
2023-11-18 22:18:44 +0630

    implement theme provider & cmt out useeffect

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -2,9 +2,9 @@ import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
 	return (
-		<main className="flex justify-center text-xl items-center text-center min-h-screen text-emerald-400 gap-5">
-			<UserButton afterSignOutUrl="/" />
-			<h1 className="">Home</h1>
-		</main>
+		// <main className="flex justify-center text-xl items-center text-center min-h-screen text-emerald-400 gap-5">
+		<UserButton afterSignOutUrl="/" />
+		// <h1 className="">Home</h1>
+		// {/* </main> */}
 	);
 }
