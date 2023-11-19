3dd3616dc177d4300919be846843680e426f0f77
Akaikenlol <keithken972@gmail.com>
2023-11-20 02:00:59 +0630

    mobnav and search

---
+++ components/shared/search/GlobalSearch.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Image from "next/image";
+import { Input } from "@/components/ui/input";
+
+const GlobalSearch = () => {
+	return (
+		<div className="w-full max-w-[600px] max-lg:hidden relative">
+			<div className="background-light800_darkgradient relative flex min-h-[56px] grow items-center px-4 rounded-lg gap-1">
+				<Image
+					src="/assets/icons/search.svg"
+					alt="search"
+					width={24}
+					height={24}
+					className="cursor-pointer"
+				/>
+				<Input
+					type="text"
+					placeholder="Search globally"
+					value=""
+					className="no-focus paragraph-regular placeholder background-light800_darkgradient border-none shadow-none outline-none"
+				/>
+			</div>
+		</div>
+	);
+};
+
+export default GlobalSearch;
