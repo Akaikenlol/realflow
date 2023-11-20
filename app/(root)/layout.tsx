43318aa965b6cf3710bcec126ae90c5b7b459c94
Akaikenlol <keithken972@gmail.com>
2023-11-20 23:31:36 +0630

    Difference between leftsidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,20 +1,22 @@
 import Navbar from "@/components/shared/navbar/Navbar";
 import React from "react";
 import "../../styles/theme.css";
-import LeftSideBar from "@/components/shared/leftsidebar/LeftSideBar";
+import LeftSideBar from "@/components/shared/LeftSideBar";
+// import LeftSideBar from "@/components/shared/leftsidebar/LeftSideBar";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<main className="background-light850_dark100 relative ">
 			<Navbar />
-			<div className="flex justify-start items-center">
+			<div className="flex">
+				{/* <LeftSideBar /> */}
 				<LeftSideBar />
 				<section className="flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
 					<div className="mx-auto w-full max-w-5xl">{children}</div>
 				</section>
 				Right Side Bar
 			</div>
-			Toaster
+			{/* Toaster */}
 		</main>
 	);
 };
