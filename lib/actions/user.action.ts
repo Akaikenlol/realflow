54575ed77c313370c12df870289b15504ee2cfbf
Akaikenlol <keithken972@gmail.com>
2023-11-28 20:16:03 +0630

    web hook user action

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -2,6 +2,13 @@
 
 import User from "@/database/user.model";
 import { connectToDB } from "../mongoose";
+import {
+	CreateUserParams,
+	DeleteUserParams,
+	UpdateUserParams,
+} from "./shared.types";
+import { revalidatePath } from "next/cache";
+import Question from "@/database/question.model";
 
 export async function getUserById(params: any) {
 	try {
@@ -12,6 +19,67 @@ export async function getUserById(params: any) {
 		const user = await User.findOne({ clerkId: userId });
 
 		return user;
+	} catch (error) {
+		console.log("error", error);
+		throw error;
+	}
+}
+
+export async function createUser(userData: CreateUserParams) {
+	try {
+		connectToDB();
+
+		const newUser = await User.create(userData);
+
+		return newUser;
+	} catch (error) {
+		console.log("error", error);
+		throw error;
+	}
+}
+
+export async function updateUser(params: UpdateUserParams) {
+	try {
+		connectToDB();
+
+		const { clerkId, updateData, path } = params;
+
+		await User.findOneAndUpdate({ clerkId }, updateData, { new: true });
+
+		revalidatePath(path);
+	} catch (error) {
+		console.log("error", error);
+		throw error;
+	}
+}
+
+export async function deleteUser(params: DeleteUserParams) {
+	try {
+		connectToDB();
+
+		const { clerkId } = params;
+
+		const user = User.findOneAndDelete({ clerkId });
+
+		if (!user) {
+			throw new Error("User not found");
+		}
+		// Delete user from database
+		// and question, answers, comments, etc..
+
+		// get user questions ids
+		const userQuestionIds = await Question.find({ author: user._id }).distinct(
+			"_id"
+		);
+
+		// delete user questions
+		await Question.deleteMany({ author: user._id });
+
+		//To Do: delete user answers, comments, etc.
+
+		const deletedUser = await User.findByIdAndDelete(user._id);
+
+		return deletedUser;
 	} catch (error) {
 		console.log("error", error);
 		throw error;
