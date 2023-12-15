48dd40866a2937ac6f7d967e04f155f59e531cf7
Akaikenlol <keithken972@gmail.com>
2023-12-15 21:00:34 +0630

    feat: GlobalResult[Loading]

--- components/shared/search/GlobalSearch.tsx
+++ components/shared/search/GlobalSearch.tsx
@@ -1,8 +1,46 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import { Input } from "@/components/ui/input";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { formUrlQuery, removeKeysFromQuery } from "@/lib/utils";
+import GlobalResult from "./GlobalResult";
 
 const GlobalSearch = () => {
+	const router = useRouter();
+	const pathname = usePathname();
+	const searchParams = useSearchParams();
+
+	const query = searchParams.get("q");
+
+	const [search, setSearch] = useState(query || "");
+	const [isOpen, setIsOpen] = useState(false);
+
+	useEffect(() => {
+		const delayDebounceFn = setTimeout(() => {
+			if (search) {
+				const newUrl = formUrlQuery({
+					params: searchParams.toString(),
+					key: "global",
+					value: search,
+				});
+				router.push(newUrl, { scroll: false });
+			} else {
+				if (query) {
+					const newUrl = removeKeysFromQuery({
+						params: searchParams.toString(),
+						keysToRemove: ["global", "type"],
+					});
+					router.push(newUrl, { scroll: false });
+				}
+			}
+		}, 300);
+
+		return () => clearTimeout(delayDebounceFn);
+	}, [search, searchParams, router, pathname, query]);
+
+	// console.log(query);
 	return (
 		<div className="w-full max-w-[600px] max-lg:hidden relative">
 			<div className="background-light800_darkgradient relative flex min-h-[56px] grow items-center px-4 rounded-lg gap-1">
@@ -15,10 +53,18 @@ const GlobalSearch = () => {
 				/>
 				<Input
 					type="text"
+					value={search}
+					onChange={(e) => {
+						setSearch(e.target.value);
+
+						if (!isOpen) setIsOpen(true);
+						if (e.target.value === "" && isOpen) setIsOpen(false);
+					}}
 					placeholder="Search globally"
-					className="no-focus paragraph-regular placeholder background-light800_darkgradient border-none shadow-none outline-none"
+					className="no-focus paragraph-regular placeholder text-dark400_light900 background-light800_darkgradient border-none shadow-none outline-none"
 				/>
 			</div>
+			{isOpen && <GlobalResult />}
 		</div>
 	);
 };
